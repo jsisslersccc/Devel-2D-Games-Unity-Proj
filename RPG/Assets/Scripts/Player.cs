e7f85b5d336d71c3d9bb86843ba1e877a3a6c573
John Sissler <jsissler@gmail.com>
2019-11-21 20:09:35 -0500

    End of Chapter 6.

--- RPG/Assets/Scripts/Player.cs
+++ RPG/Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : Character
     public void Start()
 	{
         hitPoints.value = startingHitPoints;
-        //inventory = Instantiate(inventoryPrefab);
+        inventory = Instantiate(inventoryPrefab);
         healthBar = Instantiate(healthBarPrefab);
         healthBar.character = this;
 	}
